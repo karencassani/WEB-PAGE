4fd3830366d13ff7fc1046f0d2de055c2b0beaee
karencassani <a01640798@tec.mx>
2026-04-09 23:32:14 -0600

    update

--- App.jsx
+++ App.jsx
@@ -1,13 +1,12 @@
-import {useState} from "react"
+import { useState } from "react";
 import "./App.css";
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 
 function App() {
-  const [cout, setCount] = useState(0)
+  const [cout, setCount] = useState(0);
   return (
     <>
-      <h1>hola mundo</h1>
       <Navbar>
         <Hero />
       </Navbar>
